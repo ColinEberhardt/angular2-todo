7e25b27f24ff908d092a76f5df9ce92b68aabc7c
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-23 11:35:49 +0000

    proper two-way binding

--- src/component/todolist/todolist.ts
+++ src/component/todolist/todolist.ts
@@ -1,18 +1,15 @@
-import {Component} from 'angular2/angular2';
+import {Component, FORM_DIRECTIVES} from 'angular2/angular2';
 
 @Component({
   selector: 'todo-list',
   templateUrl: 'component/todolist/todolist.html',
-  styleUrls: ['component/todolist/todolist.css']
+  styleUrls: ['component/todolist/todolist.css'],
+  directives: [FORM_DIRECTIVES]
 })
 export default class ToDoList {
   newItem = 'test';
 
   addClicked() {
     console.log('Add clicked', this.newItem);
-  }
-
-  newItemChanged(value: string): void {
-    this.newItem = value;
   }
 }
