321433941ec5862ababda647860603785feba31a
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-23 10:56:21 +0000

    Enabled web components

--- src/component/todolist/todolist.ts
+++ src/component/todolist/todolist.ts
@@ -1,9 +1,11 @@
-import {Component} from 'angular2/angular2';
+import {Component, ViewEncapsulation} from 'angular2/angular2';
 
 @Component({
   selector: 'todo-list',
   templateUrl: 'component/todolist/todolist.html',
-  styleUrls: ['component/todolist/todolist.css']
+  styleUrls: ['component/todolist/todolist.css'],
+  encapsulation: ViewEncapsulation.Native
 })
 export default class ToDoList {
+  greeting: String = 'Hello World';
 }
