f6349175aeae1825adab9b4a47a089b7bdd0160d
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-23 09:59:21 +0000

    todolist starter

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,4 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
-import Greeting from './greeting/greeting';
+import ToDoList from './todolist/todolist';
 
-bootstrap(Greeting);
+bootstrap(ToDoList);
---
+++ src/app/todolist/todolist.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: 'app/todolist/todolist.html',
+  styleUrls: ['app/todolist/todolist.css']
+})
+export default class ToDoList {
+}
