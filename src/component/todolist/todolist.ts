b584f0081e54e750407e6f954830b72b7d2ca8db
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-23 10:56:26 +0000

    Revert "Enabled web components"
    This reverts commit 2584496ed391f5124e0a11664d1a336fa3fb6ba6.

--- src/component/todolist/todolist.ts
+++ src/component/todolist/todolist.ts
@@ -1,11 +1,9 @@
-import {Component, ViewEncapsulation} from 'angular2/angular2';
+import {Component} from 'angular2/angular2';
 
 @Component({
   selector: 'todo-list',
   templateUrl: 'component/todolist/todolist.html',
-  styleUrls: ['component/todolist/todolist.css'],
-  encapsulation: ViewEncapsulation.Native
+  styleUrls: ['component/todolist/todolist.css']
 })
 export default class ToDoList {
-  greeting: String = 'Hello World';
 }
