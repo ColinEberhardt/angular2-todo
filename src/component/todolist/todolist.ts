bc0a0a00da7d18569d2efc56061e402d497f4572
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-23 11:52:22 +0000

    Added a todo store

--- src/component/todolist/todolist.ts
+++ src/component/todolist/todolist.ts
@@ -1,13 +1,17 @@
-import {Component, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Component, FORM_DIRECTIVES, CORE_DIRECTIVES} from 'angular2/angular2';
+import {TodoStore} from './../../store/todoStore';
+
+const store = new TodoStore();
 
 @Component({
   selector: 'todo-list',
   templateUrl: 'component/todolist/todolist.html',
   styleUrls: ['component/todolist/todolist.css'],
-  directives: [FORM_DIRECTIVES]
+  directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 export default class ToDoList {
   newItem = 'test';
+  items = store.items;
 
   addItem() {
     console.log('Add item', this.newItem);
