1bba25033461e736c614507691833cde77b4fb50
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-11-24 10:25:37 +0000

    Make todoItem immutable

--- src/component/todoitem/todoitem.ts
+++ src/component/todoitem/todoitem.ts
@@ -1,10 +1,11 @@
-import {Component, Input, Output, EventEmitter} from 'angular2/angular2';
+import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from 'angular2/angular2';
 import {TodoItem as ToDoItemModel} from './../../store/todoStore';
 
 @Component({
   selector: 'todo-item',
   templateUrl: 'component/todoitem/todoitem.html',
-  styleUrls: ['component/todoitem/todoitem.css']
+  styleUrls: ['component/todoitem/todoitem.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class TodoItem {
   @Input()
