dcae71449ee593d269e91eb9148bb1d4578476cb
Colin Eberhardt <colin.eberhardt@gmail.com>
2015-12-16 17:21:56 +0000

    Enable web components

--- src/app/todolist/todolist.ts
+++ src/app/todolist/todolist.ts
@@ -1,9 +1,10 @@
-import {Component} from 'angular2/core';
+import {Component, ViewEncapsulation} from 'angular2/core';
 
 @Component({
   selector: 'todo-list',
   templateUrl: 'app/todolist/todolist.html',
-  styleUrls: ['app/todolist/todolist.css']
+  styleUrls: ['app/todolist/todolist.css'],
+  encapsulation: ViewEncapsulation.Native
 })
 export default class ToDoList {
 }
